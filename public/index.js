ced6023747d378dae087d20cb1f6059aa7cdef30
chaya phuwichit <chaya@odds.team>
2024-08-09 18:09:38 +0000

    init

---
+++ public/index.js
@@ -0,0 +1,12 @@
+const url = '/comments';
+
+fetch(url)
+  .then((r) => r.json())
+  .then(
+    (json) =>
+      (document.getElementById('output').innerHTML = JSON.stringify(
+        json,
+        null,
+        2
+      ))
+  );
