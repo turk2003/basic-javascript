b8ebffc631b685892012935092bb918401c27229
chaya phuwichit <chaya@odds.team>
2024-08-10 09:45:54 +0000

    Craete-GetAll

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const Members =[];
+let Members = [];
 
 const appHeader = document.getElementById('app-header');
 
@@ -10,36 +10,38 @@ const lbFirstName = document.getElementById('lbFirstName');
 const lbLastName = document.getElementById('lbLastName');
 const lbEmail = document.getElementById('lbEmail');
 
-let btnSubmit = document.getElementById('btnSubmit');
+let btnGetAll = document.getElementById('btnGetAll');
 let btnAdd = document.getElementById('btnAdd');
 const tbMembers = document.getElementById('tbMembers');
 
-function members(firstname , lastname , email){
+
+function members(firstname, lastname, email) {
     return {
         firstname: firstname,
         lastname: lastname,
         email: email
     };
 }
-function deleteMember(id){
-    Members.splice(id,1);
+function deleteMember(id) {
+    console.log("Delete#"+id);
+    //Members.splice(id, 1);
     renderTable();
 }
 
 
 
-function renderTable(){
+function renderTable() {
     tbMembers.innerHTML = '';
     let delBtn = document.createElement('button');
     delBtn.innerHTML = 'Delete';
     delBtn.className = 'btn btn-danger';
-    
+
     for (let i = 0; i < Members.length; i++) {
 
         delBtn.onclick = () => {
-            deleteMember(i);
+            deleteMember(Members[i].id);
         };
-        
+
         let newRow = `<tr id=ID${i}>
                         <th scope="row">${i}</th>
                         <td id="lbFirstName${i}">${Members[i].firstname}</td>
@@ -56,12 +58,59 @@ function renderTable(){
 }
 
 let memberID = 0;
-btnAdd.onclick = () => {
-    let member = members(txtFirstName.value, txtLastName.value, txtEmail.value);
-    Members.push(member);
+
+
+
+async function getData() {
+    const url = "http://localhost:8000/member";
+    let response = {};
+    try {
+        response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        return json;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+async function getMembers() {
+    Members = await getData();
+    // console.log(Members);
     renderTable();
-    memberID++;
+}
+
+async function addMember(member) {
+    let request = new Request("http://localhost:8000/member", {
+        method: "POST",
+        body: JSON.stringify(member),
+    });
+
+    let response = await fetch(request);
+    let result = await response.json();
+    console.log(result);
+
+    getMembers();
+
+}
+
+
+
+btnGetAll.onclick = () => {
+    getMembers();
 };
 
-appHeader.innerHTML = 'Hello from JavaScript!';
-
+btnAdd.onclick = () => {
+    let member = members(txtFirstName.value, txtLastName.value, txtEmail.value);
+    addMember(member);
+    //Members.push(member);
+    //renderTable();
+    memberID++;
+};
